6a9dff367a9524ca11dca044e0cf9a64516ff22c
Abhishek Kashyap <a.kashyap0009@gmail.com>
2021-01-17 10:43:14 +0530

    analyze_tv_data.py

--- analyze_tv_data.py
+++ analyze_tv_data.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 # Load the CSV data into DataFrames
-super_bowls = pd.read_csv("datasets/super_bowls.csv")
-tv = pd.read_csv("datasets/tv.csv")
-halftime_musicians = pd.read_csv("datasets/halftime_musicians.csv")
+super_bowls = pd.read_csv("super_bowls.csv")
+tv = pd.read_csv("tv.csv")
+halftime_musicians = pd.read_csv("halftime_musicians.csv")
 
 # Display the first five rows of each DataFrame
 display(super_bowls.head())
